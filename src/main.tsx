3d6df1237d2342b7576eaff64c2dfd9b73a44748
Shubham Barathe <shubham.barathe@gmail.com>
2025-05-13 22:25:50 +0530

    Header changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // REQUIRED for navbar toggler
 import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
